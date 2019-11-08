ec4d6fbe6bf974f8ec20b08ab116ade7c5205ffa
Steven <steventruong1995@yahoo.com>
2019-11-08 11:04:37 -0500

    change header.js

--- twitterworkshop/src/components/Header.js
+++ twitterworkshop/src/components/Header.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+    // render() {
+    //     return (
+    //         <div>
+    //             hello, {this.props.name}
+    //         </div>
+    //     );
+    // }
+
+    constructor() {
+        super(props);
+
+        this.state = {
+            species: [],
+            fetch: false,
+            loading: false
+        }
+    }
+
+    componentWillMount() {
+        this.setState({
+            loading: true
+        })
+
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+            .then(res => res.json())
+            .then(response => {
+                this.setState({
+                    species: response.results,
+                    loading: true,
+                    fetched: true
+                })
+            })
+    }
+
     render() {
-        return (
-            <div>
-                hello, {this.props.name}
-            </div>
-        );
+        return <div>hello, {this.props.name}</div>
     }
 }
+
+
 
 export default Header;
